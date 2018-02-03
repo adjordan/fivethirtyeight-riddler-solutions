742a605b83b20d3261da01b8ba6963806518a3f3
= <adj6193@gmail.com>
2018-02-02 19:18:12 -0500

    update variable name

--- 02-02-2018/express.py
+++ 02-02-2018/express.py
@@ -2,12 +2,12 @@ import numpy as np
 
 
 num_each = 30
-n_iter = 10000
+n_iter = 100000
 
 
 def main():
 	total_preferred_vitamins_count = 0
-	for i in range(iterations):
+	for i in range(n_iter):
 		vitamins_in_bottle = np.append(np.zeros(num_each), np.ones(num_each))
 		np.random.shuffle(vitamins_in_bottle)
 		preferred_vitamins_count = 0
@@ -21,7 +21,7 @@ def main():
 
 		total_preferred_vitamins_count += preferred_vitamins_count
 
-	print(total_preferred_vitamins_count/iterations/num_each)
+	print(total_preferred_vitamins_count/n_iter/num_each)
 
 if __name__ == "__main__":
 	main()
